1aad70f98c339ec3e40baa5190dd3b3764fc266c
david-cortes <david.cortes.rivera@gmail.com>
2023-07-18 21:58:02 +0200

    updates for latest cython/numpy/scipy

--- setup.py
+++ setup.py
@@ -277,7 +277,9 @@ setup(
                             install_requires = ["numpy", "cython", "scipy"],
                             define_macros = [
                                 ("FOR_PYTHON", None),
-                                ("NDEBUG", None)
+                                ("NDEBUG", None),
+                                ("CYTHON_EXTERN_C", 'extern "C"'),
+                                ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")
                             ]
                         )]
 )
